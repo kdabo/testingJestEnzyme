8e638c63c661b54b5fda8c12557b104e5d1099ea
katarina <katarina.dabo@gmail.com>
2018-11-20 17:39:51 +0100

    added integration tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -29,7 +29,7 @@ class CommentBox extends Component {
             <button>Submit a comment</button>
           </div>
         </form>
-        <button onClick={this.props.fetchComments}>Fetch comments</button>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>Fetch comments</button>
       </div>
     );
   }
